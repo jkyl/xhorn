3f8fd7798e81e09c2ef703ee3c29da6b050299e8
Jonathan Kyl <jkyl@uchicago.edu>
2016-03-30 12:24:52 -0500

    added deglitching and fixed freq axis

--- jkyl_spec.py
+++ jkyl_spec.py
@@ -10,15 +10,26 @@ import struct
 
 class Spec:
     
-    def __init__(self, acc_len = 1, samp_rate = None, ip = '128.135.52.192'):
+    def __init__(self, acc_len = 1, samp_rate = None, deglitch = True,
+                 ip = '128.135.52.192', boffile = 'simple_spec.bof'):
         '''
+        Inputs: 
+            acc_len (seconds), samp_rate (MHz), ip (string), boffile (string)
+
+        Initializes a 2048 channel spectrometer by connecting to the given IP, 
+        loading the given .bof file, setting (or estimating) the sample rate, 
+        setting the accumulation length, setting FFT shift, and arming PPS. 
         '''
         self._n_chans = 2048
-        self._boffile = 'simple_spec.bof'
         print('\nConnecting to "{}"'.format(ip))
         self.connect(ip)
-        print('Loading .bof file "{}"'.format(self._boffile))
-        self.load_bof()
+        if deglitch:
+            print('Loading .bof file "adc5g_test_rev2.bof.gz"')
+            self.load_bof('adc5g_test_rev2.bof.gz')
+            print('Deglitching')
+            self.deglitch()
+        print('Loading .bof file "{}"'.format(boffile))
+        self.load_bof(boffile)
         print('Setting sample rate to {} MHz'.format(samp_rate))
         self.set_clock(samp_rate)
         print('Setting accumulation length to {} second(s)'.format(acc_len))
@@ -39,12 +50,29 @@ class Spec:
             print('Roach not connected. Exiting...\n')
             sys.exit()
   
-    def load_bof(self):
+    def load_bof(self, boffile):
         '''
         '''
-        self.roach.progdev(self._boffile)
+        self.roach.progdev(boffile)
+        self._boffile = boffile
         time.sleep(0.5)
         
+    def deglitch(self):
+        '''
+        '''
+        zdok = 0
+        snap_name = 'scope_raw_0_snap'
+        try:
+            adc.set_test_mode(self.roach, zdok)
+            adc.sync_adc(self.roach)
+            opt0, glitches0 = adc.calibrate_mmcm_phase(self.roach, zdok, 
+                                                       [snap_name,])
+            adc.unset_test_mode(self.roach, zdok)
+            
+        except RuntimeError:
+            print('Deglitching failed: check clock source\n')
+            sys.exit()
+            
     def set_clock(self, samp_rate = None):
         '''
         '''
@@ -62,7 +90,7 @@ class Spec:
         '''
         self._acc_len = acc_len
         self.roach.write_int('acc_len',
-            int(round(acc_len * self._samp_rate * 1e6 / (16 * self._n_chans))))
+            int(round(acc_len * self._samp_rate * 1e6 / (16. * self._n_chans))))
         
     def set_fft_shift(self, bitstring):
         '''
@@ -98,19 +126,19 @@ class Spec:
     def snap_time(self):
         '''
         '''
-        return self._snap('snapshot_adc0', 'b', man_trig = True)
+        return self._snap('snapshot_adc0', fmt = 'b', man_trig = True)
         
     def freq_axis(self):
         '''
         '''
-        return np.linspace(0, self._samp_rate // 2, self._n_chans + 1)[1:]
+        return np.arange(0, self._samp_rate / 2., self._samp_rate / 2. / self._n_chans)
 
     def plot_spec(self):
         '''
         '''
-        f, a = self.freq_axis, 10 * np.log10((self.snap_spec())
-        p = plt.plot(f, a)
-        plt.xlim((0, f[-1])
+        f, db = self.freq_axis(), 10 * np.log10(self.snap_spec())
+        p = plt.plot(f, db)
+        plt.xlim((0, f[-1]))
         plt.xlabel('Frequency (MHz)')
         plt.ylabel('Power (dB)')
         return p
