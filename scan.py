8311f5e80ec3e4fc4024c3af24a175afaacd05dd
Jonathan Kyl <jkyl@uchicago.edu>
2016-04-04 00:28:36 -0500

    default fname is output/[y_m_d_h_m].csv

--- scan.py
+++ scan.py
@@ -1,12 +1,14 @@
 from spec import Spec
 from motor import Motor
 from optparse import OptionParser
+from time import localtime as t
 import numpy as np
 import sys
 import csv
 
 
-def snap_and_move(m, s, wr, v = True, step = 1, pos = 0, fname = 'data.csv'):
+
+def snap_and_move(m, s, wr, v = True, step = 1, pos = 0, fname = 'output/test.csv'):
     '''
     '''
     if v: print('Integrating')
@@ -19,7 +21,7 @@ def snap_and_move(m, s, wr, v = True, step = 1, pos = 0, fname = 'data.csv'):
     return pos
 
         
-def go(m, s, v = True, home = 0, step = 1, bound = 60, fname = 'data.csv'):
+def go(m, s, v = True, home = 0, step = 1, bound = 60, fname = 'output/test.csv'):
     '''
     '''
     f = open(fname, 'wb')
@@ -29,12 +31,15 @@ def go(m, s, v = True, home = 0, step = 1, bound = 60, fname = 'data.csv'):
     if v: print('Homing')
     m.move(abst = home)
     pos = 0
-    while True:
-        while pos < bound:
-            pos = snap_and_move(m, s, wr, v = v, step = step, pos = pos, fname = fname)
-        while pos > -bound:
-            pos = snap_and_move(m, s, wr, v = v, step = -step, pos = pos, fname = fname)
-            
+    try:
+        while True:
+            while pos < bound:
+                pos = snap_and_move(m, s, wr, v = v, step = step, pos = pos, fname = fname)
+            while pos > -bound:
+                pos = snap_and_move(m, s, wr, v = v, step = -step, pos = pos, fname = fname)
+    except (KeyboardInterrupt, SystemExit):
+        print('Stopped')
+        sys.exit()
     
 if __name__ == '__main__':
     p = OptionParser()
@@ -55,8 +60,8 @@ if __name__ == '__main__':
     p.add_option('-p', '--port',
         dest = 'port', type = 'str', default = '/dev/tty.usbserial-AD01XAOK', 
         help = 'Set dev port for VXM controller. Defaults to /dev/tty.usbserial-AD01XAOK.')
-    p.add_option('-d', '--degrees',
-        dest = 'degrees', type = 'float', default = 1, 
+    p.add_option('-d', '--degs',
+        dest = 'degs', type = 'float', default = 1, 
         help = 'Set angular step in degrees. Defaults to 1.')
     p.add_option('-z', '--zero',
         dest = 'zero', type = 'float', default = 0, 
@@ -67,16 +72,18 @@ if __name__ == '__main__':
     p.add_option('-g', '--go',
         dest = 'go', action = 'store_true', default = False, 
         help = 'Execute data run. False by default.')
+    t = t()
     p.add_option('-f', '--fname',
-        dest = 'fname', type = 'str', default = 'data.csv', 
-        help = 'Set output filename. Defaults to data.csv.')
+                 dest = 'fname', type = 'str', default = 'output/{}.csv'.format(
+                     '_'.join([str(i) for i in (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min)])),
+                 help = 'Set output filename. Defaults to output/[y_m_d_h_m].csv.')
     
     opts, args = p.parse_args(sys.argv[1:])
     m = Motor(port = opts.port)
     s = Spec(ip = opts.ip, samp_rate = opts.samp_rate,
              acc_len = opts.acc_len, v = not opts.q)
     if opts.go:
-        go(m, s, v = not opts.q, step = opts.degrees,
+        go(m, s, v = not opts.q, step = opts.degs,
            home = opts.zero, bound = opts.bound, fname = opts.fname)
 
     
