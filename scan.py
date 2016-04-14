7dcc8d6d3b737a904d258b2465fbbaa682418f4f
Jonathan Kyl <jkyl@uchicago.edu>
2016-04-14 11:31:37 -0500

    hdf5 -> h5 extension

--- scan.py
+++ scan.py
@@ -37,14 +37,14 @@ def snap_and_move(m, s, f, pos, acc_len = 1, step = 1, n_accs = 10, dt = 0):
         sys.exit()
         
 def go(step = 1, home = 0, bound = 60, samp_rate = 4800, acc_len = 1, n_accs = 10,
-       port = '/dev/tty.usbserial-AD01XAOK', fname = 'output/test.hdf5', ip = '128.135.52.192'):
+       port = '/dev/tty.usbserial-AD01XAOK', fname = 'output/test.h5', ip = '128.135.52.192'):
     '''
     Main function that creates motor, spec, and hdf5 objects, calculates the computer's offset
     from true time, and calls snap_and_move() in order to sweep the horn through a range of
     elevations and write accumulations and metadata to disk. 
 
     Inputs:
-        Step size in degrees, home angle in degrees, max angle in degrees, sample rate in MHz, 
+        Step size in degrees, home angle in degrees, upper bound angle in degrees, sample rate in MHz, 
         accumulation length in seconds, number of accumulations, /dev address of motor controller, 
         output filename, and ip address of roach board. 
 
@@ -79,19 +79,20 @@ if __name__ == '__main__':
     args = sys.argv
     if len(args) == 3 and args[2] == 'go':
         
-    # edit defaults here
+    ### edit defaults here ###
         go(
             step = 1, 
             home = 0,
             bound = 60,
             fname = args[1],
             ip = '128.135.52.192',
+            port = '/dev/tty.usbserial-AD01XAOK'
             samp_rate = 4800,
             acc_len = 1,
             n_accs = 20
         )
     else:
-        print('\nUsage: "python scan.py <output/fname.hdf5> go"')
+        print('\nUsage: "python scan.py <output/fname.h5> go"')
     
     
 
