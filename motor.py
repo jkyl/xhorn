345663383b71e1ecd884f4271b7372b2ab54f066
Jonathan Kyl <jkyl@uchicago.edu>
2016-04-16 18:59:29 -0500

    Added default acceleration and speed properties and docstrings

--- motor.py
+++ motor.py
@@ -8,7 +8,6 @@ INCR  = 'I1M'     # increment steps
 ABS   = 'IA1M'    # set absolute position
 POS   = 'X'       # query position
 
-
 def gen_serial_obj(port = '/dev/ttyUSB0', baudrate = 38400):
     '''
     Creates a serial object with the specified baudrate and /dev location. 
@@ -51,48 +50,25 @@ def send_command(ser, cmd):
         raise
         
 class Motor:
-    '''
-    Class that uses the previous functions for homing, moving, and querying. Takes 
-    optional /dev directory and baudrate args. 
-
-        - position() method returns float of current position. 
-
-        - status() method returns 'busy', 'ready', or 'jog mode'.
-
-        - set_baudrate() method takes baudrate arg and sends command to change
-          the vxm baudrate, then generates a new serial object with the same br.
-          Recommended that you then use the save_settings() method so the vxm 
-          wont re-initialize to the the old br when you turn it on again. 
-
-        - save_settings() method saves the current settings, returns bool of 
-          success. 
-
-        - home() method takes no args and executes sequence for returning home.
-          Returns bool of success.
-
-        - incr() method increments the motor by a degree amount (must be a multiple
-          of 0.01 degs), and returns bool of success. 
-
-        - abst() method moves the motor to an absolute degree position (must be a 
-          multiple of 0.01 degs), and returns bool of success. 
-
-        - send() method takes any command string and returns the raw writeback.
-
-        - baudrate property returns integer of current baudrate.
-
-    '''
     def __init__(self, port = '/dev/ttyUSB0', baudrate = 38400):
+        '''
+        '''
         self._ser = gen_serial_obj(port, baudrate)
         self._ser.open()
-        
+        self.accl = 1
+        self.speed = 20
+    
     def position(self):
-        return int(self.send("X").replace("X", '')[:-1]) / 100.
-
-    @property
-    def baudrate(self):
-        return self._ser.baudrate
+        '''
+        Queries the motor and returns a float of its position in degrees (rounded to 
+        two decimal places).
+        '''
+        return round(int(self.send("X").replace("X", '')[:-1]) / 100.), 2)
 
     def status(self):
+        '''
+        Returns 'busy', 'ready', or 'jog mode'.
+        '''
         rv = self.send('V')
         if rv == 'B':
             return 'busy'
@@ -102,9 +78,15 @@ class Motor:
             return 'jog mode'
 
     def send(self, cmd):
+        '''
+        Calls send_command() and returns writeback. 
+        '''
         return send_command(self._ser, cmd)
 
     def set_baudrate(self, baudrate):
+        '''
+        Sets the controller's baudrate. Use save_settings() to retain for next time. 
+        '''
         if self.send("F C setB{}, R".format(str(baudrate)[:2])) == '^':
             self._ser.baudrate = baudrate
             return True
@@ -112,21 +94,65 @@ class Motor:
             return False
 
     def save_settings(self):
+        '''
+        Saves the motor's settings like baudrate. 
+        '''
         return self.send('rsm') == '^'
             
     def home(self):
-        return self.send("C S1M600, I1M-0, I1M4000, I1M-0,  IA1M-0, R C"+\
+        '''
+        Executes a homing sequence using the magnetic limit switch. 
+        '''
+        return self.send("F C S1M600, I1M-0, I1M4000, I1M-0, IA1M-0, R C"+\
                          "S1M600, I1M-0, I1M4000, I1M-0, IA1M-0, R") == '^'
-    
-    def incr(self, degs, accl = 1, speed = 20):
-        degs = round(degs, 2)
-        assert(degs != 0)
-        cmd = INIT + ACCL + str(accl)+ ',' + SPEED + str(speed * 100)\
-              + ',' + INCR + str(degs * 100) + ',' + RUN
+        
+    def _move(self, degs, incr = None, abst = None, accl = None, speed = None):
+        '''
+        Formats and sends a string for either absolute or relative stepping. 
+        '''
+        if incr:
+            TYPE = INCR
+        elif abst:
+            TYPE = ABST
+        if accl is None:
+            accl = self._accl
+        if speed is None:
+            speed = self._speed
+        cmd = ','.join((INIT + ACCL + '{}', SPEED + '{}', TYPE  + '{}', RUN))\
+                 .format(int(accl), int(speed * 100), int(degs * 100))
         return self.send(cmd) == '^'
         
-    def abst(self, degs, accl = 1, speed = 20):
-        degs = round(degs, 2)
-        cmd = INIT + ACCL + str(accl)+ ',' + SPEED + str(speed * 100)\
-              + ',' + ABS + str(degs * 100) + ',' + RUN
-        return self.send(cmd) == '^'
+    def abst(self, degs, accl = None, speed = None):
+        '''
+        Moves the motor to an absolute position of <degs>, takes optional speed 
+        (degs/s) and accl (1 - 127) args. 
+        '''
+        return self._move(degs, abst = True, accl = accl, speed = speed)
+
+    def incr(self, degs, accl = None, speed = None):
+        '''
+        Moves the motor to a relative position of <degs>, takes optional speed 
+        (degs/s) and accl (1 - 127) args. 
+        '''
+        return self._move(degs, incr = True, accl = accl, speed = speed)
+
+    @property
+    def baudrate(self):
+        '''
+        Getter for serial baudrate. 
+        '''
+        return self._ser.baudrate
+
+    @property
+    def accl(self):
+        '''
+        Setter/getter for acceleration (1 - 127).
+        '''
+        return self.accl
+
+    @property
+    def speed(self):
+        '''
+        Setter/getter for speed (deg/s).
+        '''
+        return self.speed
