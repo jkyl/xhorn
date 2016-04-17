8a48bf251ed4e281c8cc8cbb16c0e2e635c372b7
Jonathan Kyl <jkyl@uchicago.edu>
2016-04-16 20:24:37 -0500

    query position in inner loop of snap_and_move(), switched to zenith, min and max (instead of home and bound) and true homing

--- scan.py
+++ scan.py
@@ -6,82 +6,78 @@ import numpy as np
 import sys, csv, time, h5py
 
     
-def snap_and_move(m, s, f, pos, acc_len = 1, step = 1, n_accs = 10, dt = 0):
+def snap_and_move(m, s, f, zenith = 0, acc_len = 1, step = 1, n_accs = 10, dt = 0):
     '''
-    Function that gets called recursively in go(). 
+    Function that gets called recursively in go(). Takes a snapshot, calculates true time 
+    based on offset, queries the motor position, calls io.write_to_hdf5 on the hdf5 file 
+    object, then moves the motor.
 
     Inputs:
-        Required - Motor and Spec objects, hdf5 file object, position of motor.
-        Optional - accumulation length in secs, step size in degrees, number of 
-                   accumulations, computer's offset from true utc time in secs.
-
+        Required - Motor, Spec, and  hdf5 file objects.
+        Optional - zenith angle relative to 0 (degs), accumulation length in secs, step size 
+                   in degrees, number of accumulations, computer's offset from true utc time 
+                   in secs.
     Outputs:
-        Takes a snapshot, calculates true time based on offset, calls io.write_to_hdf5 on 
-        the hdf5 file object, then moves the motor and returns its position if the move was 
-        succesful. 
+        None, writes to disk. 
     '''
-    print('Writing {} {}s integrations:'.format(n_accs, acc_len))
+    print('Integrating...')
     for i in range(n_accs):
         print(i + 1)
         spec = s.snap_spec()
         true_time = ts.true_time(dt)
+        pos = m.position() - zenith
         io.write_to_hdf5(f, spec, {'angle_degs': pos,
                                    'utc_time': true_time,
                                    'samp_rate_mhz': s.samp_rate})
     print('Moving {} deg'.format(step))
-    if m.incr(step):
-        print('Sleeping {}s'.format(acc_len))
-        time.sleep(acc_len)
-        return m.position()
-    else:
+    if not m.incr(step):
+        print('Move failed')
         sys.exit()
         
-def go(step = 1, home = 0, bound = 60, samp_rate = 4400, acc_len = 1, n_accs = 20,
-       port = '/dev/ttyUSB0', ip = '128.135.52.192'):
+def go(step = 1, min = 0, max = 60, zenith = 0, samp_rate = 4400, acc_len = 1, n_accs = 20,
+       port = '/dev/ttyUSB0', ip = '128.135.52.192', home = True):
     '''
     Main function that creates motor, spec, and hdf5 objects, calculates the computer's offset
     from true time, and calls snap_and_move() in order to sweep the horn through a range of
     elevations and write accumulations and metadata to disk. Closes file and creates a new file
-    after each return to home.
+    after each return to zenith.
 
     Inputs:
-        Step size in degrees, home angle in degrees, upper bound angle in degrees, sample rate in MHz, 
-        accumulation length in seconds, number of accumulations, /dev address of motor controller, 
-        output filename, and ip address of roach board. 
+        Step size in degrees, zenith angle in degrees, min and max angles in degrees, sample 
+        rate in MHz, accumulation length in seconds, number of accumulations, /dev address of 
+        motor controller, output filename, and ip address of roach board. 
 
     Outputs: 
-        None, but writes to file. Fails safe by closing the file. 
+        None,  writes to disk. Fails safe by closing the file. 
     '''
+    m = Motor(port = port)
+    s = Spec(ip = ip, samp_rate = samp_rate, acc_len = acc_len)
+    dt = ts.offset()
+    fname = 'output/' + ts.true_time(dt) + '.h5'
     try:
-        dt = ts.offset()
-        fname = 'output/' + ts.true_time(dt) + '.h5'
         f = h5py.File(fname, 'w')
         print('Opened file "{}"'.format(fname))
-        m = Motor(port = port)
-        s = Spec(ip = ip, samp_rate = samp_rate, acc_len = acc_len)
-        print('Homing to absolute position of {} deg'.format(home))
-        if m.abst(home):
-            pos = 0
-            while True:
-                while pos < bound:
-                    pos = snap_and_move(m, s, f, pos, step = step,
-                                        acc_len = acc_len, n_accs = n_accs, dt = dt)
-                while pos > 0:
-                    pos = snap_and_move(m, s, f, pos, step = -step,
-                                        acc_len = acc_len, n_accs = n_accs, dt = dt)
-                f.close()
-                print('Closed file "{}"'.format(fname))
-                dt = ts.offset()
-                fname = 'output/' + ts.true_time(dt) + '.h5'
-                f = h5py.File(fname, 'w')
-                print('Opened file "{}"'.format(fname))
-                
-    except (KeyboardInterrupt, SystemExit):
-        print('\nKeyboardInterrupt / SystemExit\n')
-    except Exception as e1:
-        print('\nExiting with unexpected exception:\n')
-        raise e1
-    f.close()
+        print('Homing')
+        if home:
+            m.home()
+        print('Moving to zenith ({} degs relative to zero)'.format(zenith))
+        m.abst(zenith)
+        while True:
+            while m.position() - zenith + step <= max:
+                snap_and_move(m, s, f, zenith = zenith, acc_len = acc_len,
+                              step = step, n_accs = n_accs, dt = dt)
+            while m.position() - zenith - step >= min:
+                snap_and_move(m, s, f, zenith = zenith, acc_len = acc_len,
+                              step = -step, n_accs = n_accs, dt = dt)   
+            f.close()
+            print('Closed file "{}"'.format(fname))
+            dt = ts.offset()
+            fname = 'output/' + ts.true_time(dt) + '.h5'
+            f = h5py.File(fname, 'w')
+            print('Opened file "{}"'.format(fname))
+    except:
+        f.close()
+        raise
         
     
 if __name__ == '__main__':
@@ -91,13 +87,15 @@ if __name__ == '__main__':
     ### edit defaults here ###
         go(
             step = 1, 
-            home = 0,
-            bound = 60,
+            min = 0,
+            max = 60,
+            zenith = 0,
             samp_rate = 4400,
             acc_len = 1,
             n_accs = 20,
             port = '/dev/ttyUSB0',
-            ip = '128.135.52.192',      
+            ip = '128.135.52.192',
+            home = True
         )
     else:
         print('\nUsage: "python scan.py go"')
