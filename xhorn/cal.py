0d03a84dd5c74e03f36e2fea330de74aae157c94
Jonathan Kyl <jkyl@Jonathans-MacBook-Air.local>
2016-06-24 21:46:13 -0500

    cal

--- xhorn/cal.py
+++ xhorn/cal.py
@@ -4,7 +4,7 @@ import numpy as np
 def mask(specs, angles):
     cal_specs, cal_angs = (i[np.where(angles==CAL_ANG)] for i in (specs, angles))
     
-    return s, a
+    return cal_specs, cal_angs
     
 def tsys(specs, angles):
     pass
