271ae780f095db184345ec01ac76a2babb61fbe3
Christopher Sheehy <csheehy@uchicago.edu>
2016-07-28 18:28:08 -0500

    change in outputs of loader

--- xhorn/lab_spec.py
+++ xhorn/lab_spec.py
@@ -1,32 +1,68 @@
 import numpy as np
 import os
+import time
 from tqdm import trange
 from datetime import datetime
+from sig_gen import Gen
 
-def saver(s, path=None):
+def integrate(s, int_time=10, LO_freq=9.5, LO_time=None, fname=None):
     '''
-    Snaps and saves a spectrum to an .npz file with the specified path. 
-    '''
-    if path is None:
-        path = '/'.join(os.path.abspath(__file__).split('/')[:-2]\
+    Integrates for the specified interval in seconds and saves to path. 
+    ''' 
+    #Calculate number of integrations
+    n_ints = int(round(float(int_time)/s.acc_len))
+    assert(n_ints>0)
+
+    #Determine LO switching
+    specs = np.zeros((n_ints, 2048))
+    times = np.zeros(n_ints)
+    t0 = time.time()
+    if not LO_freq is None:
+        g = Gen()
+        g.set_pow(10) #10 dBm output power
+        g.set_rf(True) #Turn on
+
+        #Create array of LO frequencies over time
+        if type(LO_freq) in (float, int):
+            LO_freq = np.array([LO_freq] * n_ints)
+            
+        elif (type(LO_time) in (float, int)) and hasattr(LO_freq, '__iter__'):
+            LO_freq = np.array(LO_freq)
+            n_at_each = int(round(float(LO_time)/s.acc_len))
+            assert(n_at_each>0)
+            LO_freq = np.tile(np.repeat(LO_freq, n_at_each),
+                              int(round(float(n_ints / n_at_each))))[:n_ints]
+
+        #Snap and stack spectra
+        for i in trange(n_ints):
+            if i==0 or (LO_freq[i-1] != LO_freq[i]):
+                g.set_freq(LO_freq[i])
+                time.sleep(s.acc_len)
+            specs[i] = s.snap_spec()
+            times[i] = time.time() - t0
+    else:
+        for i in trange(n_ints):
+            specs[i] = s.snap_spec()
+            times[i] = time.time() - t0
+        
+    #Find absolute path to data dir and save
+    if fname is None:
+        fname = '/'.join(os.path.abspath(__file__).split('/')[:-2]\
         + ['lab_spec_data/{}']).format(datetime.now().isoformat()[:-7])
-    np.savez_compressed(path, s.snap_spec(),
+    np.savez_compressed(fname, spec=specs, LO_freq=LO_freq, time=times,
                         **{k: v for k, v in s.__dict__.items() if k!= '_roach'})
+        
 
-def scanner(s, nscan=10):
+def loader(fname):
     '''
-    Loops with a progress bar over nscans, taking spectra and saving with saver().
-    Filenames are local time isoformatted strings. 
+    Returns: when given a valid .npz filename.
+    spec
+    LO_freq
+    time
+    metadata
     '''
-    for i in trange(nscan):
-        saver(s)
-
-def loader(path):
-    '''
-    Returns a (spec, metadata) tuple when given a valid .npz filepath.
-    '''
-    d = {k: v for k, v in np.load(path).iteritems()}
-    return d.pop('arr_0'), {k: v.item() for k, v in d.items()}
+    d = {k: v for k, v in np.load(fname).iteritems()}
+    return d.pop('spec'), d.pop('LO_freq'), d.pop('time'), {k: v.item() for k, v in d.items()}
         
         
         
